a494262cb6590522012044a462fde6e2a50e1499
dennetwebdesigner <dennetwebdesigner@gmail.com>
2022-06-02 18:54:57 -0300

    commented and typed page login and login submit

--- frontend/src/pages/LoginPage/LoginSubmit.tsx
+++ frontend/src/pages/LoginPage/LoginSubmit.tsx
@@ -1,10 +1,10 @@
-import { AxiosResponse } from 'axios';
-import { Dispatch as ReactDispatch, SetStateAction } from 'react';
+import { Dispatch as ReactDispatch, MouseEvent, SetStateAction } from 'react';
 import { NavigateFunction } from 'react-router-dom';
 import { AnyAction, Dispatch } from 'redux';
 
 import { api } from '../../config/api';
 
+// interface that received the data type
 interface iLoginSubmit {
   username: string;
   password: string;
@@ -12,8 +12,10 @@ interface iLoginSubmit {
   setUsername: ReactDispatch<SetStateAction<string>>;
   setPassword: ReactDispatch<SetStateAction<string>>;
   navigate: NavigateFunction;
+  event: MouseEvent;
 }
 
+// shot action submit form for user login
 export const LoginSubmit = async ({
   username,
   password,
@@ -21,16 +23,23 @@ export const LoginSubmit = async ({
   setUsername,
   setPassword,
   navigate,
+  event,
 }: iLoginSubmit): Promise<void> => {
+  // stop the refresh action
+  event.preventDefault();
+
+  // validate if username and password are empty
   if (!username || !password) alert('Os campos devem ser preenchidos corretamente!');
 
+  // Query api to confirm credentials
   const user = await api.post('http://localhost:3333/auth', {
     username,
     password,
   });
 
+  // validate if exist data and if exist the token
   if (user.data && user.data.token) {
-    console.log(user.data.token);
+    // set new data to state auth redux
     dispatch({
       type: 'addAuth',
       payload: {
@@ -40,8 +49,11 @@ export const LoginSubmit = async ({
       },
     });
 
+    // clear fields username and password
     setUsername('');
     setPassword('');
+
+    // change page login for dashBoard
     navigate('/');
   }
 };
--- frontend/src/pages/LoginPage/index.tsx
+++ frontend/src/pages/LoginPage/index.tsx
@@ -1,6 +1,6 @@
 import './LoginPage.css';
 
-import React, { useEffect, useState } from 'react';
+import React, { MouseEvent, useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -10,78 +10,81 @@ import { LoginSubmit } from './LoginSubmit';
 
 export const LoginPage = (): JSX.Element => {
   // Nome de usuário e senha
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState<string>('');
   const [password, setPassword] = useState('');
-
+  // type input text or password
   const [typePassword, setTypePassword] = useState('password');
+
+  // case auth allowed  move for DashBoard
   const navigate = useNavigate();
 
-  // const auth = useSelector((state: any) => state.auth.auth);
+  // dispatach redux used for auth
   const dispatch = useDispatch();
 
   return (
-    <>
-      <div className="login-container">
-        <div className="login-form-control">
-          <h1 className="login-tittle">Finantrol</h1>
-          <h2 className="login-subttitle">Seja Bem-Vindo ao seu controle financeiro!</h2>
+    <main className="login-container">
+      <section className="login-form-control">
+        <h1 className="login-tittle">Finantrol</h1>
+        <h2 className="login-subttitle">Seja Bem-Vindo ao seu controle financeiro!</h2>
 
-          <div className="login-group-form-control">
-            <div className="login-group-form">
-              <label htmlFor="username">Nome de Usuário</label>
+        <form className="login-group-form-control">
+          <article className="login-group-form">
+            <label htmlFor="username">Nome de Usuário</label>
+            <input
+              type="text"
+              id="username"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+          </article>
+          <article className="login-group-form">
+            <label htmlFor="password">Senha</label>
+            <input
+              type={typePassword}
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <div className="login-show-password">
               <input
-                type="text"
-                id="username"
-                value={username}
-                onChange={(e) => setUsername(e.target.value)}
-              />
-            </div>
-            <div className="login-group-form">
-              <label htmlFor="password">Senha</label>
-              <input
-                type={typePassword}
-                id="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-              <div className="login-show-password">
-                <input
-                  type="checkbox"
-                  id="show-password"
-                  onClick={() => {
-                    if (typePassword === 'password') setTypePassword('text');
-                    else setTypePassword('password');
-                  }}
-                />
-                <label htmlFor="show-password">mostrar senha</label>
-              </div>
-            </div>
-            <div className="login-group-form">
-              <button
-                onClick={(e) => {
-                  LoginSubmit({
-                    username,
-                    password,
-                    dispatch,
-                    setUsername,
-                    setPassword,
-                    navigate,
-                  });
+                type="checkbox"
+                id="show-password"
+                onClick={() => {
+                  if (typePassword === 'password') setTypePassword('text');
+                  else setTypePassword('password');
                 }}
-              >
-                Entrar
-              </button>
+              />
+              <label htmlFor="show-password">mostrar senha</label>
             </div>
-            <Link to="/signup" className="page-signup-button">
-              <p>cadastrar</p>
-            </Link>
-          </div>
-          <div className="google-button">
-            <img className="google-icon" src={buttonGoogle} alt="" />
-            <p>Fazer login com Google</p>
-          </div>
+          </article>
+          <article className="login-group-form">
+            <button
+              type="submit"
+              onClick={(e: MouseEvent) => {
+                LoginSubmit({
+                  event: e,
+                  username,
+                  password,
+                  dispatch,
+                  setUsername,
+                  setPassword,
+                  navigate,
+                });
+              }}
+            >
+              Entrar
+            </button>
+          </article>
+          <Link to="/signup" className="page-signup-button">
+            <p>cadastrar</p>
+          </Link>
+        </form>
+
+        <div className="google-button">
+          <img className="google-icon" src={buttonGoogle} alt="" />
+          <p>Fazer login com Google</p>
         </div>
-      </div>
-    </>
+      </section>
+    </main>
   );
 };
